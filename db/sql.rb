6c70530d5e13d334033318d9f366cfc0dd4f0ee1
Harry A. Swan <has.land@hotmail.co.uk>
2016-05-27 15:41:27 +0100

    autodeploy

--- db/sql.rb
+++ db/sql.rb
@@ -4,10 +4,12 @@ class SQLRun
     def self.exec(sql)
         begin
             # db = PG.connect({ dbname: 'codebook', host: 'localhost' })
-            db = PgoG.connect( {dbname: 'd2ipapqomj5dq5', host: 'ec2-54-243-201-116.compute-1.amazonaws.com', port: 5432, user: 'gmwruaareztcpe', password: 'mAR_K_AuMf62uge-Kr43vgUyQw'})
+            db = PG.connect( {dbname: 'd2ipapqomj5dq5', host: 'ec2-54-243-201-116.compute-1.amazonaws.com', port: 5432, user: 'gmwruaareztcpe', password: 'mAR_K_AuMf62uge-Kr43vgUyQw'})
             result = db.exec(sql)
         ensure
             db.close
+
+        rescue NoMethodError
         end
         return result
     end
