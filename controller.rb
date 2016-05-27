ff64216b0d78f271e55cc408beb47fd12009f132
Harry A. Swan <has.land@hotmail.co.uk>
2016-05-27 13:35:26 +0100

    converting to heroku

--- controller.rb
+++ controller.rb
@@ -1,6 +1,5 @@
 require( 'sinatra' )
 require( 'sinatra/contrib/all' )
-require('pry-byebug')
 
 require_relative('db/sql')
 
--- db/sql.rb
+++ db/sql.rb
@@ -3,7 +3,8 @@ class SQLRun
 
     def self.exec(sql)
         begin
-            db = PG.connect({ dbname: 'codebook', host: 'localhost' })
+            # db = PG.connect({ dbname: 'codebook', host: 'localhost' })
+            db = PG.connect( {dbname: 'dbifvpla7mc8n1', host: 'ec2-54-228-189-127.eu-west-1.compute.amazonaws.com', port: 5432, user: 'tblvokekcplfws', password: 'O9bs5kJ9gT3Hrf8eMd-a9H3XSd'})
             result = db.exec(sql)
         ensure
             db.close
