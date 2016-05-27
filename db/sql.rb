a3fdf3a2e08e18b0cf4369bc5b3404b5c6f25c6c
Harry A. Swan <has.land@hotmail.co.uk>
2016-05-27 14:57:44 +0100

    autodeploy

--- db/sql.rb
+++ db/sql.rb
@@ -4,7 +4,7 @@ class SQLRun
     def self.exec(sql)
         begin
             # db = PG.connect({ dbname: 'codebook', host: 'localhost' })
-            db = PG.connect( {dbname: 'd2ipapqomj5dq5', host: 'ec2-54-243-201-116.compute-1.amazonaws.com', port: 5432, user: 'gmwruaareztcpe', password: 'mAR_K_AuMf62uge-Kr43vgUyQw'})
+            db = PgoG.connect( {dbname: 'd2ipapqomj5dq5', host: 'ec2-54-243-201-116.compute-1.amazonaws.com', port: 5432, user: 'gmwruaareztcpe', password: 'mAR_K_AuMf62uge-Kr43vgUyQw'})
             result = db.exec(sql)
         ensure
             db.close
